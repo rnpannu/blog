d0779868f728f601fea703a42e74092caa9b7432
Manraj Pannu <90409868+rnpannu@users.noreply.github.com>
2025-02-03 19:29:36 -0700

    Add files via upload

---
+++ blog/src/main/java/com/mspannu/blog/services/CategoryService.java
@@ -0,0 +1,12 @@
+package com.mspannu.blog.services;
+
+import com.mspannu.blog.domain.entities.Category;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CategoryService {
+    List<Category> listCategories();
+    Category createCategory(Category category);
+    void deleteCategory(UUID id);
+}
